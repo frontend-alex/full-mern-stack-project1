33c3f317495420ec991ee9f4317ef098b41603f4
Aleksander Ivanov <alexmmech@gmail.com>
2023-04-24 22:09:25 +0300

    fixing the design on the edit page

--- client/src/Routes/UpdateProfile.js
+++ client/src/Routes/UpdateProfile.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-
 import { AiOutlineEdit } from "react-icons/ai";
 import { IoReturnDownBackOutline } from "react-icons/io5";
 
@@ -10,8 +9,7 @@ import OkToast from "../Constants/ToastMessages/OkToast";
 import useGetData from "../Hooks/useGetData";
 
 const UpdateProfile = ({ user }) => {
-
-  const { userId } = useParams()
+  const { userId } = useParams();
 
   const [values, setValues] = useState({
     username: user.username,
@@ -20,8 +18,8 @@ const UpdateProfile = ({ user }) => {
     bio: "Hello there, i love...",
   });
 
-  const [ error, setError ] = useState("");
-  const [ toast, setToast ] = useState(false)
+  const [error, setError] = useState("");
+  const [toast, setToast] = useState(false);
 
   const { updateProfile } = useGetData();
 
@@ -33,24 +31,23 @@ const UpdateProfile = ({ user }) => {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    updateProfile(values.userPfp, values.username, values.bio, userId)
-        .then(res => {
-            if(res.status == 404){
-              setError(res.error)
-            } else if(res.status == 201){
-              setToast(prev => !prev)
-              setError(res.message)
-              setTimeout(() => {
-                window.location.href = `/dashboard/${user.username}`
-              }, 3000)
-            }
-        })
+    updateProfile(values.userPfp, values.username, values.bio, userId).then(
+      (res) => {
+        if (res.status == 404) {
+          setError(res.error);
+        } else if (res.status == 201) {
+          setToast((prev) => !prev);
+          setError(res.message);
+          setTimeout(() => {
+            window.location.href = `/dashboard/${user.username}`;
+          }, 3000);
+        }
+      },
+    );
   };
 
-
   return (
-      <div className="edit-profile-container">
-
+    <div className="edit-profile-container">
       {toast && <OkToast text={error} />}
 
       {/* custom component */}
@@ -81,34 +78,41 @@ const UpdateProfile = ({ user }) => {
             </p>
           </div>
 
-          <div className="user-preview">
-            <img src={values.userPfp} />
-            <div className="user-text">
-              <h3>{values.username}</h3>
-              <span>{values.bio}</span>
+          <div className="edit-profile-pos">
+            <div className="user-preview">
+              <img src={values.userPfp} />
+              <div className="user-text">
+                <h3>{values.username}</h3>
+                <span>{values.bio}</span>
+              </div>
             </div>
+
+            <div className="input-container">
+              <input
+                type="text"
+                required
+                onChange={onChangeHandler}
+                name="userPfp"
+              />
+              <label>Profile Picture</label>
+            </div>
+
+            <div className="input-container">
+              <input
+                type="text"
+                required
+                onChange={onChangeHandler}
+                name="bio"
+              />
+              <label>Bio</label>
+            </div>
+
+            {/* {error && <p className="err-msg">{error}</p>} */}
+
+            <button className="button-regi" type="submit">
+              Save Changes
+            </button>
           </div>
-
-          <div className="input-container">
-            <input
-              type="text"
-              required
-              onChange={onChangeHandler}
-              name="userPfp"
-            />
-            <label>Profile Picture</label>
-          </div>
-
-          <div className="input-container">
-            <input type="text" required onChange={onChangeHandler} name="bio" />
-            <label>Bio</label>
-          </div>
-
-          {/* {error && <p className="err-msg">{error}</p>} */}
-
-          <button className="button-regi" type="submit">
-            Save Changes
-          </button>
         </form>
       </div>
     </div>
