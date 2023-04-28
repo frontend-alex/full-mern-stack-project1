9c1d4981cbbfd80d79d9c5c9fef47951c0e7bacd
Aleksander Ivanov <alexmmech@gmail.com>
2023-04-28 23:00:58 +0300

    started a catalog

--- client/src/Routes/Catalog.js
+++ client/src/Routes/Catalog.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+
+import { BsPlay } from "react-icons/bs";
 
 const Catalog = () => {
   return (
-    <div>Catalog</div>
-  )
-}
+    <div className="catalog-container">
+      <div className="catalog-header">
+        <h1>
+          Premuim Shoes from From Manifacturer B. Brand at reasonable prices
+        </h1>
+        <div className="box-2">
+          <div className="header-circle">
+            <BsPlay className="icon" />
+            <p>Watch a company presentation</p>
+          </div>
+        </div>
+      </div>
 
-export default Catalog
+      <div className="catalog-sorting-conainter">
+        <div className="box-1"></div>
+        <div className="box-2"></div>
+        <div className="box-3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Catalog;
